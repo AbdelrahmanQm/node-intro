b160763c775fc90e016532e29f0d68c8e555a89d
Abdelrahman Qassem <abdelrahman.qassem@gmail.com>
2023-04-09 02:45:46 +0200

    finalized node intro

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const http = require("http");
+const url = require("url");
+const fs = require("fs");
 
 // Creating a server
 const server = http.createServer((req, res) => {
-  res.end("Hello from the server");
+  const pathName = req.url;
+  switch (pathName) {
+    case "/":
+      res.end("This is OverView");
+      break;
+    case "/product":
+      res.end("This is product");
+      break;
+    case "/api":
+      fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) => {
+        const productData = JSON.parse(data);
+        res.writeHead(200, { "Content-type": "application/json" });
+        res.end(data);
+      });
+      break;
+    default:
+      res.end("Error not found");
+  }
 });
 
 // Server listening
